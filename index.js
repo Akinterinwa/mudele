7d5618ef53ac51ad17521ebbcb1161b0f2b01971
Akinterinwa Oluwanifemi <118985490+Akinterinwa@users.noreply.github.com>
2024-01-21 18:52:11 +0100

    additional work

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-    document.getElementById('playIcon').addEventListener('click', function() {
-        var audio = document.getElementById('pronunciationAudio');
-        audio.play();
-    });
+document.getElementById('playIcon').addEventListener('click', function() {
+    var audio = document.getElementById('pronunciationAudio');
 
-    // Script to handle playback when the icon is clicked 
+    // Check if the audio is paused or ended before playing
+    if (audio.paused || audio.ended) {
+        // Toggle the controls attribute to ensure playback on iOS
+        audio.controls = true;
+        // Play the audio
+        audio.play();
+    } else {
+        // Pause the audio if it's already playing
+        audio.pause();
+    }
+});
