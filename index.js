fa666cc7d1cf14c48e1d437d05b115353b6e889f
Akinterinwa Oluwanifemi <118985490+Akinterinwa@users.noreply.github.com>
2024-01-21 02:35:43 +0100

    finished working on the experience section also made some adjustment to the site, and embeded an audio for name pronouciation

---
+++ index.js
@@ -0,0 +1,6 @@
+    document.getElementById('playIcon').addEventListener('click', function() {
+        var audio = document.getElementById('pronunciationAudio');
+        audio.play();
+    });
+
+    // Script to handle playback when the icon is clicked 
